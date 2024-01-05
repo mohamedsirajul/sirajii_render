8a88637b9d0638bcabe5cf221f303aa23d8933b1
mohamedsirajul <mohamedsirajulmuneer88@gmail.com>
2024-01-05 09:37:28 +0530

    secc commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/siraji')
-def hello_world():
-    return 'Hello Sirajulllll!'
+# @app.route('/siraji')
+# def hello_world():
+#     return 'Hello Sirajulllll!'
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
+
+
+print('mohameddd sirajulll')
