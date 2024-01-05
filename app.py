3896ab8ff58050437b83fe76bac8b680cdddef8d
mohamedsirajul <mohamedsirajulmuneer88@gmail.com>
2024-01-05 09:40:36 +0530

    seccsss commit

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def sirajull():
     return 'Hello Sirajulllll!'
 
+@app.route('/sirajulllls')
+def sirajull():
+    return 'Hello Sirajulllll!'
+
 if __name__ == '__main__':
     app.run()
