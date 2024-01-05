d989a226fba1bfb1d141cbf99499eda30fa75863
mohamedsirajul <mohamedsirajulmuneer88@gmail.com>
2024-01-05 10:47:11 +0530

    fifth commit

--- app.py
+++ app.py
@@ -1,15 +1,84 @@
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
+import random
+import json
+import pickle
+import numpy as np
+# import wikipedia
+import nltk
+import cv2
+from nltk.stem import WordNetLemmatizer
+import tensorflow as tf
+from tensorflow import keras
+import atexit  # Add this line
 from flask import Flask
+from flask_cors import CORS
+import cv2
+import numpy as np
+from tkinter import *
+
+
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/siraji')
-def sirajull():
-    return 'Hello Sirajulllll!'
+lemmatizer = WordNetLemmatizer()
 
-@app.route('/sirajulllls')
+# Load the chatbot model and intents data
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+chatbot_model = keras.models.load_model('chatbotmodel.h5')
 
-def sirajii():
-    return 'MOHAMED SIRAJIII!'
 
+def clean_up():
+    cv2.destroyAllWindows()
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = chatbot_model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_input = data['message']
+    ints = predict_class(user_input)
+    res = get_response(ints, intents)
+
+    return jsonify({'response': res})
+
+
+# Run the Flask app
 if __name__ == '__main__':
-    app.run()
+    atexit.register(clean_up)  # Register cleanup function
+    app.run(port=5000)
